7851a0cb49e8df0c9f302c1d6461e7c9bdd37d49
Ian Eure <ian.eure@gmail.com>
2010-02-19 08:46:49 -0500

    Update to Cassandra 0.5.0 interface.

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@
 #
 # Cassandra
 #
-# © 2009 Digg, Inc. All rights reserved.
+# © 2009, 2010 Digg, Inc. All rights reserved.
 # Author: Ian Eure <ian@digg.com>
 #
 
 from setuptools import setup, find_packages
 
 setup(name="Cassandra",
-      version='svn816386',
+      version='0.5.0',
       description="Thrift client for Cassandra",
       url="http://incubator.apache.org/cassandra/",
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
