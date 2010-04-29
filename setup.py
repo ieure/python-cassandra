b9760d595de15928f4b0176b8f6a38efa87e1baf
Ian Eure <ian.eure@gmail.com>
2010-04-29 08:30:49 -0700

    Update setup.py to remove dependency_links, there's a usable Thrift in PyPI now.

--- setup.py
+++ setup.py
@@ -1,22 +1,19 @@
 # -*- coding: utf-8 -*-
 #
-# Cassandra
-#
 # © 2009, 2010 Digg, Inc. All rights reserved.
 # Author: Ian Eure <ian@digg.com>
 #
 
 from setuptools import setup, find_packages
 
-setup(name="Cassandra",
+setup(name="python-cassandra",
       version='0.5.0',
       description="Thrift client for Cassandra",
       url="http://incubator.apache.org/cassandra/",
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
       author="Ian Eure",
-      author_email="ian@digg.com",
+      author_email="ian.eure@gmail.com",
       scripts=['scripts/Cassandra-remote'],
       keywords="database cassandra",
-      install_requires=['Thrift'],
-      dependency_links=["http://github.com/ieure/python-cassandra/downloads"])
+      install_requires=['Thrift'])
